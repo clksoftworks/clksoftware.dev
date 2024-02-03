7128da82830ba0158e52be5df8035ee15eb77975
Chris Keefer <42353349+chrislkeefer@users.noreply.github.com>
2024-02-03 15:17:27 -0600

    fix: return not found from api response

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -5,14 +5,14 @@ import type { NextRequest } from 'next/server'
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-  let responseText = 'Hello World'
+  let responseText = 'Forbidden'
 
   // In the edge runtime you can use Bindings that are available in your application
   // (for more details see:
   //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
   //    - https://developers.cloudflare.com/pages/functions/bindings/
   // )
-  //
+  
   // KV Example:
   // const myKv = process.env.MY_KV
   // await myKv.put('suffix', ' from a KV store!')
