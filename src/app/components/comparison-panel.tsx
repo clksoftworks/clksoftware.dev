1f8d912e5d472f33733d71845d59d5fe7ef49814
Chris Keefer <42353349+chrislkeefer@users.noreply.github.com>
2024-05-05 10:15:07 -0500

    enhance: comparison panel backdrop

--- src/app/components/comparison-panel.tsx
+++ src/app/components/comparison-panel.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
-import comparisonPanelImage from "../../../public/comparison-panel.png";
+import comparisonPanelBackdropImage from "../../../public/comparison-panel-backdrop.png";
 import ComparisonPanelJourney from "./journey/comparison-panel-journey";
 
 const ComparisonPanel = ({ className }: { className: string }) => {
   return (
-    <section className={`${className} relative grid`} id="about">
-      <div className="border-black border-2 shadow-lg bg-[#EFE8DB] text-black py-8 px-6 md:px-24 z-10 relative mx-2">
+    <section
+      className={`${className} relative grid pb-4 overflow-hidden`}
+      id="about"
+    >
+      <div className="border-black border-2 shadow-lg bg-[#EFE8DB] text-black py-8 px-4 md:px-24 z-10 relative mx-4">
         <h2 className="text-4xl uppercase mb-8">
           What sets us <span className="underline">apart</span>
         </h2>
@@ -29,6 +32,13 @@ const ComparisonPanel = ({ className }: { className: string }) => {
       <div className="hidden md:block absolute -top-60 -left-36 z-0">
         <ComparisonPanelJourney />
       </div>
+
+      <Image
+        src={comparisonPanelBackdropImage}
+        fill
+        alt="Comparison Panel"
+        className="absolute !top-4 !left-6 !right-0 !-bottom-6 object-cover !w-auto"
+      />
     </section>
   );
 };
